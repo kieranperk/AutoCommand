97877b501657a2e0ebc8e6de400516fe108c8d2e
Kieran <kieran.perkinton@gmail.com>
2022-12-19 14:28:42 +0000

    ✨ Release v1.1.0
    + Added player amount checker
    + Added ability to send blank commands
    + Added players-needed value in config
    * Changed wait-length default value in config
    * Fixed command-interval name in config

--- src/main/java/com/mountaint144/autocommand/AutoCommand.java
+++ src/main/java/com/mountaint144/autocommand/AutoCommand.java
@@ -23,20 +23,31 @@ public final class AutoCommand extends JavaPlugin implements Listener {
         this.saveDefaultConfig();
         config = this.getConfig();
 
-        // If startup module is enabled
-        if(config.getBoolean("startup.enabled")) {
+        if (config.getBoolean("startup.enabled")) {
             // If wait length is above 0
-            if (config.getInt("startup.wait-length") >= 0) {
+            if (config.getInt("startup.wait-length") >= 0 && config.getInt("startup.command-interval") >= 0 && config.getInt("startup.players-needed") >= 0) {
                 int seconds = config.getInt("startup.wait-length");
                 // Wait specified time
                 new DelayedTask(() -> {
+                    // Check if there is enough players
+                    if (config.getInt("startup.players-needed") > 0) {
+                        if (Bukkit.getServer().getOnlinePlayers().size() < config.getInt("startup.players-needed")) {
+                            getLogger().severe("Couldn't send command as there aren't enough players online!");
+                            getLogger().severe("Check your config.yml if you think this is a mistake!");
+                            return;
+                        }
+                    }
                     // For command in commands
                     List<String> list = config.getStringList("startup.commands");
                     for(int index = 0; index < list.size(); index++) {
                         String command = list.get(index);
                         new DelayedTask(() -> {
-                            Bukkit.dispatchCommand(Bukkit.getConsoleSender(), command);
-                        }, (index) * config.getInt("startup.between-length"));
+                            if (command.equals("")) {
+                                // Do nothing
+                            } else {
+                                Bukkit.dispatchCommand(Bukkit.getConsoleSender(), command);
+                            }
+                        }, (index) * config.getInt("startup.command-interval"));
                     }
                 }, seconds);
             } else {
